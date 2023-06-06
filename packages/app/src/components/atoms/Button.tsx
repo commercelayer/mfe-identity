adb11dfb4c092f54376387e45b97c4ebf9959950
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2023-06-06 10:37:22 +0200

    chore: cleanup Button classnames

--- packages/app/src/components/atoms/Button.tsx
+++ packages/app/src/components/atoms/Button.tsx
@@ -5,7 +5,7 @@ export const Button = forwardRef<
   ButtonHTMLAttributes<HTMLButtonElement>
 >(({ children, ...props }, ref): JSX.Element => {
   const buttonCss =
-    'inline-flex items-center justify-center font-bold rounded text-sm leading-6 whitespace-nowrap transition duration-700 ease-in-out focus:outline-none focus:ring-2 focus:ring-offset-0 active:outline-none active:ring-2 active:ring-offset-0 border-primary text-white bg-primary hover:opacity-80 focus:ring-gray-300 active:ring-primary active:bg-primary py-2.5 px-12 w-full disabled:bg-gray-100 disabled:text-gray-300'
+    'w-full px-6 py-3 rounded text-sm text-center font-bold transition-opacity duration-500 focus:outline-none bg-black text-white hover:opacity-80'
 
   return (
     <button {...props} ref={ref} className={buttonCss}>
