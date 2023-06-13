2712736d12f5eeb713c2111c7e27fec2aa7cfebe
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2023-06-13 10:46:01 +0200

    fix: fix input transition after switch to ring

--- packages/app/src/components/atoms/Input.tsx
+++ packages/app/src/components/atoms/Input.tsx
@@ -19,7 +19,7 @@ export const Input = ({
   const form = useFormContext()
   const { hasError, errorMessage } = useValidationFeedback(name)
   const inputCss = cn([
-    'w-full autofill:text-base px-4 py-2.5 mt-2 rounded outline-0 border-0 !ring-inset ring-1 ring-gray-200 transition-colors duration-300 !bg-white !shadow-[0_0_0_1000px_white_inset] focus:ring-2 focus:ring-primary',
+    'w-full autofill:text-base px-4 py-2.5 mt-2 rounded outline-0 border-0 !ring-inset ring-1 ring-gray-200 transition-shadow duration-300 !bg-white !shadow-[0_0_0_1000px_white_inset] focus:ring-2 focus:ring-primary',
     hasError ? 'ring-red-400 ring-2' : ''
   ])
   return (
