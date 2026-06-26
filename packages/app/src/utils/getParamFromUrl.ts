b566d0bb71c709ffbb87b14b5aad6dc9cd10b4ca
Pier Francesco Ferrari <pierfrancesco@commercelayer.io>
2026-06-26 13:07:49 +0200

    fix: enforce url param get

--- packages/app/src/utils/getParamFromUrl.ts
+++ packages/app/src/utils/getParamFromUrl.ts
@@ -16,6 +16,14 @@ type UrlParam =
 export const getParamFromUrl = (param: UrlParam): string | null | undefined => {
   if (typeof window !== "undefined") {
     const params = new URLSearchParams(window.location.search)
+    if (param === "customerEmail") {
+      return params.get(param)?.replace(" ", "+")
+    }
+    if (param === "returnUrl") {
+      return params.get(param)?.slice(0, 1) === "/"
+        ? params.get(param)?.slice(1)
+        : params.get(param)
+    }
     return params.get(param)
   }
 }
